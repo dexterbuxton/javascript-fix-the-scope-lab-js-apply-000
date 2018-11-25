504f4a67ebf095d43fe67d570508904904765119
Dexter Buxton <dexter.buxton@gmail.com>
2018-11-25 23:44:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ function myAnimal() {
   if animal === 'dog' {
     return 'cat'
   } 
-  return myAnimal
+  return animal
 }
 
 function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  return yourAnimal
+  return animal
 }
 
 function add2(n) {
